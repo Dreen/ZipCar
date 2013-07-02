2a87a1b9e4d866187db7ac8e870c05fa3f95e15c
Dreen <dreen1@gmail.com>
2013-07-02 08:39:09 +0100

    test for coin

--- unit tests/test.coin.js
+++ unit tests/test.coin.js
@@ -4,5 +4,19 @@ Coin	= require('../coin.js');
 
 describe('Coin', function()
 {
+	var c = new Coin({
+		nominal: 0.5,
+		amount: 100
+	});
 
+	it('constructor should copy all props into the object', function()
+	{
+		assert.equal(c.getNominal(), 0.5);
+		assert.equal(c.getAmount(), 100);
+	});
+
+	it('getTotal() should calculate total money in this coin', function()
+	{
+		assert.equal(c.getTotal(), 50.0);
+	});
 });
