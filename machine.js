c5738f815467cb330985d2afe3c4899aeecd57e1
Dreen <dreen1@gmail.com>
2013-07-02 13:11:25 +0100

    implemented machine init

--- machine.js
+++ machine.js
@@ -3,9 +3,34 @@ Bank		= require('./bank.js'),
 Inventory	= require('./inventory.js');
 
 
-function Machine()
+function Machine(nominals, items)
 {
+	// for reference
+	this.nominals = nominals;
+	this.items = items;
+
+	// modify nominals so they are array values
+	for (var i=0, len=nominals.length; i<len; i++)
+		nominals[i] = [this.nominals[i]];
+
+	// create resources
+	this.bank = new Bank(nominals);
+	this.bank_buffer = new Bank(nominals);
+	this.inv = new Inventory(items);
+debugger;
+	// create state
+	this.selected = null;
+	this.screen = "Ready";
 }
 
+Machine.prototype.getSelected = function()
+{
+	return this.selected;
+};
+
+Machine.prototype.readScreen = function()
+{
+	return this.screen;
+};
 
 module.exports = Machine;
--- unit tests/test.machine.js
+++ unit tests/test.machine.js
@@ -4,75 +4,74 @@ Machine	= require('../machine.js');
 
 describe('Machine', function()
 {
-	var machine = new Machine();
-
-	// initialisation process
-
 	var nominals = [2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
-
-	it('should seed the machine with all nominals and 0 actual coins', function()
-	{
-		for (var i=0; i<nominals.length; i++)
-		{
-			assert.equal(machine.bank.byNominal(nominals[i]).getAmount(), 0);
-			assert.equal(machine.bank_buffer.byNominal(nominals[i]).getAmount(), 0);
-
-			// put some coins in for later
-			machine.bank.byNominal(nominals[i]).amount = 10;
-		}
-	});
-
-
 	var items = [
 		['A', 1.0],
 		['B', 0.2],
 		['C', 1.25],
 		['D', 0.75]
 	];
+	var machine = new Machine(nominals, items);
 
-	it('should seed the machine with all item types and 0 actual items', function()
+	describe('initialisation -', function()
 	{
-		for (var i=0; i<items.length; i++)
+		it('should seed the machine with all nominals and 0 actual coins', function()
 		{
-			assert.equal(machine.inv.byName(items[i]).getAmount(), 0);
+			for (var i=0; i<nominals.length; i++)
+			{
+				assert.equal(machine.bank.byNominal(nominals[i]).getAmount(), 0);
+				assert.equal(machine.bank_buffer.byNominal(nominals[i]).getAmount(), 0);
 
-			// put some items in for later
-			machine.inv.byName(items[i]).amount = 10;
-		}
+				// put some coins in for later
+				machine.bank.byNominal(nominals[i]).amount = 10;
+			}
+		});
+
+		it('should seed the machine with all item types and 0 actual items', function()
+		{
+			for (var i=0; i<items.length; i++)
+			{
+				assert.equal(machine.inv.byName(items[i][0]).getAmount(), 0);
+
+				// put some items in for later
+				machine.inv.byName(items[i][0]).amount = 10;
+			}
+		});
+
+		it('machine should be in "Ready" state', function()
+		{
+			assert.equal(machine.getSelected(), null);
+			assert.equal(machine.readScreen(), 'Ready');
+		});
 	});
 
-	it('machine should be in "Ready" state', function()
+	describe('purchase -', function()
 	{
-		assert.equal(machine.getSelected(), null);
-		assert.equal(machine.read(), 'Ready');
-	})
+		it('select an item - machine should make a selection and screen should display the cost', function()
+		{
+			machine.select('C');
+			assert.equal(machine.getSelected().getName(), 'C');
+			assert.equal(machine.readScreen(), 'Cost: £1.25');
+		});
 
-	// purchase process
+		it('slot a £1 coin into the machine - cost should be updated and the coin moved to the buffer', function()
+		{
+			machine.pay(1.0);
+			assert.equal(machine.bank_buffer.byNominal(1.0).getAmount(), 1);
+			assert.equal(machine.readScreen(), 'Cost: £0.25');
+		});
 
-	it('select an item - machine should make a selection and screen should display the cost', function()
-	{
-		machine.select('C');
-		assert.equal(machine.getSelected().getName(), 'C');
-		assert.equal(machine.read(), 'Cost: £1.25');
-	});
-
-	it('slot a £1 coin into the machine - cost should be updated and the coin moved to the buffer', function()
-	{
-		machine.pay(1.0);
-		assert.equal(machine.bank_buffer.byNominal(1.0).getAmount(), 1);
-		assert.equal(machine.read(), 'Cost: £0.25');
-	});
-
-	it('slot another £1 into the machine - the buffer gets emptied into the bank proper, change and item are dispenced, machine goes back to "Ready" state', function()
-	{
-		machine.pay(1.0);
-		assert.equal(machine.bank_buffer.byNominal(1.0).getAmount(), 0);
-		assert.equal(machine.bank.byNominal(1.0).getAmount(), 12);
-		assert.equal(machine.bank.byNominal(0.5).getAmount(), 9);
-		assert.equal(machine.bank.byNominal(0.2).getAmount(), 9);
-		assert.equal(machine.bank.byNominal(0.05).getAmount(), 9);
-		assert.equal(machine.inv.byName('C').getAmount(), 9);
-		assert.equal(machine.getSelected(), null);
-		assert.equal(machine.read(), 'Ready');
+		it('slot another £1 into the machine - the buffer gets emptied into the bank proper, change and item are dispenced, machine goes back to "Ready" state', function()
+		{
+			machine.pay(1.0);
+			assert.equal(machine.bank_buffer.byNominal(1.0).getAmount(), 0);
+			assert.equal(machine.bank.byNominal(1.0).getAmount(), 12);
+			assert.equal(machine.bank.byNominal(0.5).getAmount(), 9);
+			assert.equal(machine.bank.byNominal(0.2).getAmount(), 9);
+			assert.equal(machine.bank.byNominal(0.05).getAmount(), 9);
+			assert.equal(machine.inv.byName('C').getAmount(), 9);
+			assert.equal(machine.getSelected(), null);
+			assert.equal(machine.readScreen(), 'Ready');
+		});
 	});
 });
