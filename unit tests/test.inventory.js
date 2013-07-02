bfed1d8bb88f07f144f677dae3fc723f1ac04a13
Dreen <dreen1@gmail.com>
2013-07-02 09:49:38 +0100

    tests for item and inventory

--- unit tests/test.inventory.js
+++ unit tests/test.inventory.js
@@ -4,5 +4,41 @@ Inventory	= require('../inventory.js');
 
 describe('Inventory', function()
 {
+	var inv = new Inventory([
+		['A', 1.00, 10],
+		['B', 0.20, 10],
+		['C', 1.25, 10],
+		['D', 0.75, 10]
+	]);
 
+	it('should have a total of 40 items that cost £32 all together', function()
+	{
+		assert.equal(inv.getTotalAmount(), 40);
+		assert.equal(inv.getTotalCost(), 32.0);
+	});
+
+	it('get an item by name - should return A', function()
+	{
+		assert.equal(inv.byName('A').getName(), 'A');
+	});
+
+	it('remove an item from inventory - should update amount', function()
+	{
+		inv.get('A');
+		assert.equal(inv.byName('A').getAmount(), 9);
+	});
+
+	it('remove all items A and try to remove one more - should return false', function()
+	{
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		inv.get('A');
+		assert.ok(!inv.get('A'));
+	});
 });
--- unit tests/test.item.js
+++ unit tests/test.item.js
@@ -4,5 +4,32 @@ Item	= require('../item.js');
 
 describe('Item', function()
 {
+	var i = new Item('test', 1.25, 10);
 
+	it('constructor should copy all props into the object', function()
+	{
+		assert.equal(c.getName(), 'test');
+		assert.equal(c.getCost(), 1.25);
+		assert.equal(c.getAmount(), 10);
+	});
+
+	it('dispence an item', function()
+	{
+		i.dispence();
+		assert.equal(c.getAmount(), 9);
+	});
+
+	it('remove all items and try to remove one more - should return false', function()
+	{
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		i.dispence():
+		assert.ok(!i.dispence());
+	});
 });
